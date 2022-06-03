54171550500c965c9c51e23002595e978690793f
wills <ejioguvictor6@gmail.com>
2022-06-03 12:42:25 -0700

    task 4

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - a function that prints in lowercase
+ * and prints in uppercase
+ * followed by a new line
+ * Return: 0 sucessfully
+ */
+int main(void)
+{
+	int ch;
+
+	for (ch = 'a' ; ch <= 'z' ; ch++)
+		putchar (ch);
+	for (ch = 'A' ; ch <= 'Z' : ch++)
+		putchar (ch);
+	putchar ('\n');
+	return (0);
+}
