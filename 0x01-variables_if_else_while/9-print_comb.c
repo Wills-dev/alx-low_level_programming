147f0585b8b957daf5e7c72e94f14372bc07890e
wills <ejioguvictor6@gmail.com>
2022-06-03 15:11:41 -0700

    task 10

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,9 +9,14 @@ int main(void)
 {
 	int n;
 
-	for (n = 48; n < 57; n++)
+	for (n = 48; n < 58; n++)
 	{
 		putchar(n);
+		if (n ! = 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
 	}
 	putchar('\n');
 	return (0);
