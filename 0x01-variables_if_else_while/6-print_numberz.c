ccc3c261367c2057e556ca75249f8e55955e9acc
wills <ejioguvictor6@gmail.com>
2022-06-03 14:17:06 -0700

    task 7

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio>
+
+/**
+ * main - program that prints in sigle number
+ * followed by a new line
+ * Return: 0 successfully
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 48; n < 58; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
