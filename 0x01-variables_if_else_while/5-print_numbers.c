9d0b927cbde348481e6b379ab33d2bc23b5d4b81
wills <ejioguvictor6@gmail.com>
2022-06-03 13:54:59 -0700

    task 6

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - print all the single numbers
+ * followed by a new line
+ * Return: 0 successfully
+ */
+int main(void)
+{
+	int n = 0
+
+	while (n < 10)
+	{
+		printf("%d\n", n);
+		n++;
+	}
+	return (0);
+}
