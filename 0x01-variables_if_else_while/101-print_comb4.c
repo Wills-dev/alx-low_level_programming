be418d9675213b53004b781c68a96c89ec53bdcc
wills <ejioguvictor6@gmail.com>
2022-06-03 16:27:23 -0700

    task 12

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - print all possible combination in 3 digits
+ * Return: 0 successfully
+ */
+int main(void)
+{
+	int n, m, l;
+
+	for (n = 48; n < 58; n++)
+	{
+		for (n = 48; n < 58; n++)
+		{
+			for (m = 49; m < 58; m++)
+			{
+				if (l > m && m > n)
+				{
+					putchar(n);
+					putchar(m);
+					putchar(l);
+					if (n != 55 || m != 56)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+		putchar('\n');
+		return (0);
+	}
