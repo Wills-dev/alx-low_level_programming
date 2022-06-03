194a975b90957606c4b9d7039d8b5005310e8268
wills <ejioguvictor6@gmail.com>
2022-06-03 13:59:37 -0700

    task 6

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -7,12 +7,10 @@
  */
 int main(void)
 {
-	int n = 0
+	int n;
 
-	while (n < 10)
-	{
-		printf("%d\n", n);
-		n++;
-	}
+	for (n = 0; n < 10; n++)
+		printf("%d", n);
+	printf("\n");
 	return (0);
 }
