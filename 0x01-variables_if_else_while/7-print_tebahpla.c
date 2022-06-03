7fb6dd1489a84955ec94663652e10cc0601e4955
wills <ejioguvictor6@gmail.com>
2022-06-03 14:25:52 -0700

    task 8

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - print alphabets in lowercase starting from z
+ * followed by a new line
+ * Return: 0 successfully
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = z; ch >= a; ch--)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
