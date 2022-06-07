5252cda388a8b75d23d0c3c982ae897d81461aff
wills <ejioguvictor6@gmail.com>
2022-06-07 09:41:40 -0700

    third commit

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include <stdio>
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - the prototype prints a-z 10x
+ * followed by a new line
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+	char ch;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (ch = 'a'; ch <= 'z';
+				ch++)
+		{
+			_putchar(ch);
+		}
+		_putchar('\n');
+	}
+
+}
