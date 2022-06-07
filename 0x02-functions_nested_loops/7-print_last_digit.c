85c2aba2842d74f1b2e840d72f8139af79032f11
wills <ejioguvictor6@gmail.com>
2022-06-07 16:32:32 -0700

    eighth commit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -5,18 +5,18 @@
  * @r: integer input
  * Return: last digit
  */
-int print_last_digit(int)
+int print_last_digit(int r)
 {
 	int n;
 
-	if (r < 0)
-	{
-		n = -1 * (r % 10);
-	}
-	else
+	if (r >= 0)
 	{
 		n = r % 10;
 	}
-	_putchar((n % 10) + '0');
-	return (n % 10);
+	else
+	{
+		n = (r % 10) * -1;
+	}
+	_putchar(n + '0');
+	return (n);
 }
