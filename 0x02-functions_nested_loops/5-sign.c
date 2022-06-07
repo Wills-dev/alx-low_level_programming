fe132601f07536400f43b0418a257df438d0b981
wills <ejioguvictor6@gmail.com>
2022-06-07 14:26:32 -0700

    sixth commit

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_sign - function that prints the sign of a numbe1~
+ * @n: input number
+ * Description: print the sign of a number
+ * Return: 1 if number is positive
+ * 0  if number is zero and -1 if number is negative
+ */
+int print_sign(int n)
+{
+	int value;
+
+	if (n > 0)
+	{
+		value = 1;
+		_putchar('+');
+	}
+	else if (n == 0)
+	{
+		value = 1;
+		_putchar('0');
+	}
+	else
+	{
+		value = -1;
+		_putchar('-');
+	}
+	return (value);
+}
