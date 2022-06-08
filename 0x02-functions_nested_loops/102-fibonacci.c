cd911ea8fe580923df3b64a54d3a372e1141a02f
wills <ejioguvictor6@gmail.com>
2022-06-08 08:58:56 -0700

    fifteenth commit

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,27 @@
+#include <stdio>
+
+/**
+ * main - prints fibonacci numbers less or equal to 50
+ * Return: zero successfully
+ */
+int main(void)
+{
+	long int i, j, k, sum;
+
+	j = 1;
+	k = 2;
+	sum = 0;
+
+	for (i = 0; i < 50; i++)
+	{
+		printf("%ld, ", j);
+		sum = j + k;
+		j = k;
+		k = sum;
+		if (i == 48)
+		{
+			printf("%ld\n", j);
+		}
+	}
+	return (0);
+}
