d411c1d5dbaeac6c7bf2f2ba382898ba677c4918
wills <ejioguvictor6@gmail.com>
2022-06-08 07:03:44 -0700

    tenth commit

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,34 +6,32 @@
  */
 void times_table(void)
 {
-	int i, j, multiply;
+	int i, j, k;
 
-	for (i = 0; i < 9; i++)
+	for (i = 0; i < 10; i++)
 	{
-		for (j = 0; j <= 9; j++)
+		for (j = 0; j < 10; j++)
 		{
-			multiply = i * j;
+		 	k = j * i;
 			if (j == 0)
 			{
-				_putchar(multiply + '0');
+				_putchar(k + '0');
 			}
-			else
+
+			if (k < 10 && j != 0)
 			{
-				if (multiply < 10)
-				{
-					_putchar(',');
-					_putchar(32);
-					_putchar(32);
-					_putchar(multiply + '0');
-				}
-				else
-				{
-					_putchar(',');
-					_putchar(32);
-					_putchar((multiply / 10) + '0');
-					_putchar((multiply % 10) + '0';
-				}
-			};
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(k + '0');
+			}
+			else if (k >= 10)
+			{
+				_putchar(' ');
+				_putchar(' ');
+				_putchar((k / 10) + '0');
+				_putchar((k % 10) + '0');
+			}
 		}
 		_putchar('\n');
 	}
