904fa5a59dcc8c3e7ef96772ea436dccec6533bd
wills <ejioguvictor6@gmail.com>
2022-06-09 07:29:32 -0700

    first commit

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _isupper - prints upper case
+ * @c: Return 1 if c is uppercase
+ * Return: 0 if otherwise
+ */
+int _isupper(int c);
+{
+	char ch;
+
+	for (ch = 'A'; ch <= 'Z'; ch++)
+	{
+		if (c == 'C')
+		{
+			return (1);
+		}
+	}
+	return (0);
+}
