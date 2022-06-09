926bdfc620f8dc5bd39607083518384ff015d5cb
wills <ejioguvictor6@gmail.com>
2022-06-09 13:49:22 -0700

    last commit

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -9,10 +9,9 @@
  * @year: year
  * Return: void
  */
-
 void print_remaining_days(int month, int day, int year)
 {
-	if (year % 4 == 0 || ((year % 100 ==) && (year % 400 == 0)))
+	if (year % 4 == 0 || ((year % 100 == 0) && (year % 400)))
 	{
 		if (month > 2 && day >= 60)
 		{
@@ -23,7 +22,7 @@ void print_remaining_days(int month, int day, int year)
 	}
 	else
 	{
-		if (month == 2 && say == 60)
+		if (month == 2 && day == 60)
 		{
 			printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
 		}
