23e7a46a5202cbcbd18e5171ce8d14a50ba5b7b9
wills <ejioguvictor6@gmail.com>
2022-06-09 09:02:28 -0700

    fourth commit

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints number 0 to 9
+ * Return: 0 successfully
+ */
+void print_numbers(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+	{
+		_putchar(n);
+	}
+	_putchar('\n');
+	return (0);
+}
