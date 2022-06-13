1c430e131d56a414a65f03bc5abbb060d842e868
Ejiogu Victor <61105201+Wills-dev@users.noreply.github.com>
2022-06-13 19:59:36 +0100

    First commit

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * reset_to_98 -update the value of the varable
+ * the pointer points to 98
+ * @n: - pointer to the variable to update
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
