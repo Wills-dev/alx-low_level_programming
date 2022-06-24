07f114fe83e4179cfc2cdab17753ca57bec4f69c
Ejiogu Victor <61105201+Wills-dev@users.noreply.github.com>
2022-06-24 15:12:16 +0100

    new commit

---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _abs - check r
+ * @r: integer input
+ * Description: function returns absolute value of number
+ * Return: Absolute value
+ */
+int _abs(int r)
+{
+	if (r >= 0)
+	{
+		return (r);
+	}
+	else
+	{
+		return (r * -1);
+	}
+}
